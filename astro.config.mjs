7f5a36ae6f74e1118d377a6c60edffe86f00708f
Victor Aremu <me@victorare.mu>
2024-08-03 03:36:00 +0100

    refactor: update tagline
    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,24 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import favicons from "astro-favicons";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://victorare.mu",
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    favicons({
+      masterPicture: "./src/favicon.svg",
+      emitAssets: true,
+      faviconsDarkMode: true,
+    }),
+    tailwind(),
+    react(),
+  ],
   output: "server",
   adapter: netlify(),
 });
