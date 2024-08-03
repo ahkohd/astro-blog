7e70e78d73cb389ddd3c2a7087d7a1f298a12a40
Victor Aremu <me@victorare.mu>
2024-08-03 03:03:40 +0100

    feat: setup netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,15 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://victorare.mu",
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react()],
+  output: "server",
+  adapter: netlify(),
 });
+
