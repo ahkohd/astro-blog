24defddbc0c4923de0ee2d080d1b1f33b32a55aa
Victor Aremu <me@victorare.mu>
2024-08-03 05:41:27 +0100

    refactor: update site title

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "victor aremu";
+export const SITE_TITLE = "Victor Aremu";
 export const SITE_DESCRIPTION = "I'm a maker, a software engineer";
